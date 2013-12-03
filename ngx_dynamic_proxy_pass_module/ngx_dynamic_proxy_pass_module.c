7b65c9e4e7e34a2e33274274b729c60212c60503
胡鹏 <hupenghp2005@gmail.com>
2013-12-03 17:40:53 +0800

    warning

--- ngx_dynamic_proxy_pass_module/ngx_dynamic_proxy_pass_module.c
+++ ngx_dynamic_proxy_pass_module/ngx_dynamic_proxy_pass_module.c
@@ -36,8 +36,6 @@ static ngx_command_t ngx_dynamic_proxy_pass_module_commands[] = {
 	ngx_null_command
 };
 
-static ngx_int_t ngx_http_dypp_postconfig(ngx_conf_t *cf);
-
 static void* ngx_http_dypp_create_loc_conf(ngx_conf_t* cf);
 
 static char* ngx_http_dypp_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child);
